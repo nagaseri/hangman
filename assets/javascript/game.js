8baa27206d444273d6f1cdf2698d6765aa990f1d
Eri <eriinwanderland@calvisitor-10-105-149-28.calvisitor.1918.berkeley.edu>
2017-06-15 16:37:43 -0700

    adding new files css images and javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+//global variables
+
+	var words = ["cat", "dog", "hat", "top", "hot", "rot"];
+	var computerChoice = "";
+	var userChoice = "";
+	var wrongGuesses = [];
+	var guessesLeft = 10;
+	var wins = 0;
+	var losses = 0;
+
+//Computer chooses word randomly from word choice array
+	function computerMakesChoice(){
+		var randomIndex = Math.floor(Math.random () * words.length);
+		computerChoice = words.[randomIndex];
+		console.log("computerChoice", computerChoice);
+	}
+
+//function to reset game
+	function resetGame (){
+		userChoice = "";
+		guessesLeft = 10;
+		wrongGuesses = [];
+		computerMakesChoice();
+	}
+
+//Number of dashes display according to the randomized word chosen
+	" _ _ _ "
+
+//user enters a letter from letter choice array
+	//if user choice is one of the letters from the word
+		//alert: Good job
+		//number of guesses decrements
+		//used letter array increments
+		//letter shows on dashes
+	//else if letter is wrong
+		//alert: wrong letter, try again
+		//number of guesses decrements
+		//used letter array increments
+	//else if user guesses right word
+		//alert: You win!
+		//wins increment
+		//game reset
+	//else guesses left < 0
+		//losses increment
+		//game reset
